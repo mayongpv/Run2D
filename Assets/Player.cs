12ac309ad99e8c65e323698523007d12eaa4f85e
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-23 22:24:26 +0900

    타일맵 Collider 수정 완료

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,31 +1,75 @@
 ﻿using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
     Animator animator;
     Rigidbody2D rigid;
     public Vector2 jumpForce = new Vector2(0, 1000);
     public float gravityScale = 7;
-    // Start is called before the first frame update
+
     void Start()
     {
-
+        //animator = transform.Find("Sprite").GetComponent<Animator>();
         animator = GetComponentInChildren<Animator>();
         rigid = GetComponent<Rigidbody2D>();
         rigid.gravityScale = gravityScale;
         animator.Play("Run");
 
     }
-
+    public float midAirVelocity = 10;
     public float speed = 20;
     // Update is called once per frame
     void Update()
     {
+
+
         transform.Translate(speed * Time.deltaTime, 0, 0);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid.AddForce(jumpForce);
         }
+        float velocity = rigid.velocity.y;
+
+        //y 위치의 절댓값 구하는 것. 아래 3개가 다 같은 것
+        float absVelocity = Mathf.Abs(velocity);
+
+        //float absVelocity = velocity > 0 ? velocity : -velocity;
+
+        //float absVelocity = velocity;
+        //if (absVelocity>0)
+        //    absVelocity = -velocity''
+        string animationName = "";
+        if (IsGround())
+        {
+            animationName = "Run";
+        }
+        else
+        {
+            if (absVelocity < midAirVelocity)
+                animationName = "Jump_MidAir";
+
+            else if (absVelocity > 0)
+                animationName = "Jump_Up";
+
+            else //하락
+                animationName = "Jump_Fall";
+
+            animator.Play(animationName);
+        }
+    }
+
+    public Transform rayStart;
+    public float rayCheckDistance = 0.1f;
+    public LayerMask groundLayer;
+
+
+    private bool IsGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rayStart.position, Vector2.down, groundLayer);
+
+        return hit.transform != null;
+
     }
 }
---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private void Update()
+    {
+
+
+        transform.Translate(1, 0, 0, Space.Self);
+
+    
+    }
+
+
+
+}
