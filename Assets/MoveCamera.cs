001618e167b56fb6a50f760b1a4dd7cf6972bb4e
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-06-23 16:22:22 +0900

    1교시 - 신디 달려간다

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float speed = 20;
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);   
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rigid;
+    public Vector2 jumpForce = new Vector2(0, 1000);
+    public float gravityScale = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        animator = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.gravityScale = gravityScale;
+        animator.Play("Run");
+
+    }
+
+    public float speed = 20;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(jumpForce);
+        }
+    }
+}
