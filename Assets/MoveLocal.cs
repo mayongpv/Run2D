f372d7c74a9bdee0b067526d02531d94621613c5
8811 8811 <mayongpv@icloud.com>
2021-06-24 21:02:49 +0900

    오브젝트 풀 만들기 - R

---
+++ Assets/MoveLocal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveLocal : MonoBehaviour
+{
+    public Vector3 moveAxis = new Vector3(0, 1, 0);
+    public float speed = 1;
+    public float destroyTime = 5;
+
+    private void Awake()
+    {
+        Destroy(gameObject, destroyTime);
+        //5초 후에 자기 자신을 삭제
+    }
+    void Update()
+    {
+        transform.Translate(moveAxis * speed * Time.deltaTime, Space.Self);
+    }
+}
