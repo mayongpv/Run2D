9ea3e55f0e53b0eb4ab5a1001cd07d7c30dbe3f9
8811 8811 <mayongpv@icloud.com>
2021-06-24 21:14:28 +0900

    MoveLocal 도 수정하기

--- Assets/MoveLocal.cs
+++ Assets/MoveLocal.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class MoveLocal : MonoBehaviour
 {
     public Vector3 moveAxis = new Vector3(0, 1, 0);
     public float speed = 1;
     public float destroyTime = 5;
 
-    private void Awake()
+    private void OnEnable()
     {
-        Destroy(gameObject, destroyTime);
-        //5초 후에 자기 자신을 삭제
+        ObjectPool.Destroy(gameObject, destroyTime);
     }
+
     void Update()
     {
-        transform.Translate(moveAxis * speed * Time.deltaTime, Space.Self);
+        transform.Translate(moveAxis * speed * Time.deltaTime
+            , Space.Self);
     }
 }
