4376d231c5d24542cba9fd2d77517d61b8e5f987
8811 8811 <mayongpv@icloud.com>
2021-06-24 17:42:04 +0900

    git config --global user.email 으로 이메일 변경 후 test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -42,7 +42,7 @@ public class Player : MonoBehaviour
         string animationName = "";
         if (IsGround())
         {
-            animationName = "Run";
+            animationName = "Run"; 
         }
         else
         {
